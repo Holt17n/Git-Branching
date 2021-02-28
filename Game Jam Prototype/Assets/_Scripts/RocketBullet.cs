158ebb3fe0f1fb787e4911043ee3bec69580edea
davidv2022 <78017986+davidv2022@users.noreply.github.com>
2021-02-28 02:51:07 -0800

    Added player controller and projectile

---
+++ Game Jam Prototype/Assets/_Scripts/RocketBullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBullet : MonoBehaviour
+{
+    private float moveSpeed = 21f;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This destroys the bullet after 4 seconds
+        Destroy(bullet, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // moves the bullet forward
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+    }
+}
---
+++ Game Jam Prototype/Assets/_Scripts/RocketMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+    public float rotation = 90f;
+    public float moveSpeed = 7f;
+    private bool rotating;
+    public Rigidbody2D rb;
+    public GameObject player;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // The following section contains basic movement for the player
+        //Rotates player 
+        if (rotating)
+        {
+            player.transform.Rotate(0, 0, (rotation * Time.deltaTime));
+        }
+        // Moves player if key is pressed
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotating = false;
+            player.transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        }
+        // Stops rotation for next frame if key is lifted
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            rotating = true;
+        }
+
+
+        // Following section creates a projectile upon pressing down the key
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            // Creates a bullet in the middle of the rocket
+            Instantiate(bullet, player.transform.position, player.transform.rotation);
+        }
+    }
+}
