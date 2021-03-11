8a84101c495df3babef09f30d2c7198d3c6a01c7
Holt17n <78005132+Holt17n@users.noreply.github.com>
2021-03-10 22:40:43 -0800

    Level 1

---
+++ Goa Group Game/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float moveSpeed;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This destroys the bullet after 4 seconds
+        Destroy(bullet, 4);
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Check collision name
+        Debug.Log("collision name = " + col.gameObject.name);
+        if (col.gameObject.name == "EnemyCube")
+        {
+        }
+        else if (col.gameObject.name == "EnemyTurret")
+        {
+        }
+        else
+        {
+            Destroy(bullet);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // moves the bullet forward
+        transform.Translate(Vector3.right * moveSpeed* Time.deltaTime);
+    }
+}
